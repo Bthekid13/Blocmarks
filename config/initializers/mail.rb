e015e35643a41bb858e71bc4f63d66def667b092
Wil Burke <billyburke13@gmail.com>
2016-03-29 21:46:41 -0400

    restore Gemfile.lock

---
+++ config/initializers/mail.rb
@@ -0,0 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+  port:              587,
+  address:           'smtp.mailgun.org',
+  user_name:         ENV['MAILGUN_SMTP_LOGIN'],
+  password:          ENV['MAILGUN_SMTP_PASSWORD'],
+  domain:            'sandbox6a15cfd0676045d28be6d33be50d7fec.mailgun.org',
+  authentication:    :plain,
+  content_type:      'text/html'
+}
+ActionMailer::Base.delivery_method = :smtp
+
+# Makes debugging *way* easier.
+ActionMailer::Base.raise_delivery_errors = true
