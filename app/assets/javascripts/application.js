15463ce8f18103a718e5764a5a588aac60256924
Wil Burke <billyburke13@gmail.com>
2016-06-07 20:28:50 -0400

    blocmetrics api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+var blocmetrics = {}
+  blocmetrics.report = function(eventName){
+    // We create an event JSON object to send to the Blocmetrics analytics service.
+    var event = { name: eventName };
+    // # we initialize a new XMLHttpRequest() (AJAX request)
+    var request = new XMLHttpRequest();
+    // # We set the HTTP method to POST, the url of our events endpoint,
+    // # and allow the request to run asynchronously.
+    request.open("POST", "http://localhost:3000/api/events", true);
+    // # We set the request Content-Type header to 'application/json.' Without that, our
+    // # API::EventsController would not know to process the request as JSON     request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('Content-Type', 'application/json');
+    // # We send the request.
+    request.send(JSON.stringify(event));
+  };
