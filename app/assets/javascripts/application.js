d91f58e4745b53c1b27bdc8046286c58dd55d8cb
Wil Burke <billyburke13@gmail.com>
2016-06-13 16:45:40 -0400

    blocmetrics api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,14 +18,15 @@
 
 var blocmetrics = {};
   blocmetrics.report = function(eventName) {
+
     var event = {event: { name: eventName}};
 
     var request = new XMLHttpRequest();
 
-    request.open("POST", "http://salty-sailor-13.herokuapp.com/api/events", true);
+    request.open("POST", "http://localhost:3001/api/events", true);
 
     request.setRequestHeader('Content-Type', 'application/json');
 
     request.send(JSON.stringify(event));
 
- }
+ };
